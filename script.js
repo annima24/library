fb06242aef648f72e42bd37dc0e02f841c4f6e91
annima24 <nm.andrew24@gmail.com>
2022-08-10 05:18:57 -0400

    repush after corrupt message

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.display-container');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const submitButton = document.querySelector('#submit-button');
 const myLibrary = [];
+const formButton = document.querySelector('.open-form-button');
+const form = document.querySelector('#form');
 
 function Book(title, author, pages) {
   (this.title = title), (this.author = author), (this.pages = pages);
@@ -30,13 +36,33 @@ function displayBook(book) {
   card.classList.add('card');
 }
 
-const dune = new Book('dune', 'herbert', 800);
-const hobbit = new Book('the hobbit', 'tolken', 300);
-const jurassic = new Book('jurassic', 'mike c', 800);
+// const dune = new Book('dune', 'herbert', 800);
+// const hobbit = new Book('the hobbit', 'tolken', 300);
+// const jurassic = new Book('jurassic', 'mike c', 800);
 
-addBooktoLibrary(dune);
-addBooktoLibrary(hobbit);
-addBooktoLibrary(jurassic);
+// addBooktoLibrary(dune);
+// addBooktoLibrary(hobbit);
+// addBooktoLibrary(jurassic);
+
+//take values from form, create a new Book, add to myLibrary, display books.
+submitButton.addEventListener('click', function () {
+  const newBook = new Book(title.value, author.value, pages.value);
+  addBooktoLibrary(newBook);
+  clearForm();
+  container.textContent = '';
+  myLibrary.forEach((book) => displayBook(book));
+});
+
+formButton.addEventListener('click', function () {
+  console.log(form.style.display);
+});
+
+function clearForm() {
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+}
+//checks to make sure there are no empty form fields
 
 //loop over myLibrary and display the books
 myLibrary.forEach((book) => displayBook(book));
